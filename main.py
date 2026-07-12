859af84af3f87f0717c20fd452cd4ba28dc9aa5e
sanket9824188590-sys <sanket9824188590@gmail.com>
2026-07-12 15:44:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+def main():
+    print("================================")
+    print(" Welcome to Sanket AI Studio ")
+    print(" Version 1.0")
+    print("================================")
+
+if __name__ == "__main__":
+    main()
