8058f4e7f7258c92b9010565c3d52d02c06df9e5
sanket9824188590-sys <sanket9824188590@gmail.com>
2026-07-12 16:24:58 +0530

    Update main.py with File Manager

--- main.py
+++ main.py
@@ -6,12 +6,17 @@ Version : 1.0
 
 from app.logger import log
 from app.validator import validate_project
+from app.file_manager import create_folder
 
 PROJECT_DIR = "."
 
 
 def main():
     log("🚀 Starting Sanket AI Studio...")
+
+    create_folder("logs")
+    create_folder("temp")
+    create_folder("cache")
 
     validate_project(PROJECT_DIR)
 
